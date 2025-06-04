f77df5760cb37ac4b23301b1dd98213203af285f
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2025-06-05 02:02:22 +0700

    membuat route untuk post ke firebase dan menggunakan uuid sebagai id documentnya

--- app.js
+++ app.js
@@ -4,16 +4,61 @@ import { UsersService } from './services/users.service.js';
 const app = express();
 const port = 3000;
 
+const users = new UsersService();
+
 app.use(json());
 
 app.get('/api', async (req, res) => {
-  const users = new UsersService();
-  const user = await users.getDataUsers();
-  res.json(user[0]);
+  try {
+    const user = await users.getDataUsers();
+    if (user.length === 0) {
+      res.status(404).json({
+        message: 'Data Tidak Ditemukan',
+      });
+    }
+    res.status(200).json({ message: 'success', user });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Gagal Mendapatkan Data Dari Database',
+    });
+  }
 });
 
 app.post('/api', async (req, res) => {
-  const data = req.body;
+  try {
+    const data = req.body;
+    if (!data) {
+      res.status(400).json({
+        message: 'Bad Request: Data Tidak Valid',
+      });
+    }
+    const respon = await users.addDataUser(data);
+    res.status(200).json(respon);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Gagal Mengirimkan Data kedalam Database',
+    });
+  }
+});
+
+app.put('/api/:id', async (req, res) => {
+  try {
+    const data = req.body;
+    if (!data) {
+      res.status(400).json({
+        message: 'Bad Request: Data Tidak Valid',
+      });
+    }
+    const respon = await users.updateDataUser(req.params.id);
+    res.status(200).json(respon);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Gagal Mengirimkan Data kedalam Database',
+    });
+  }
 });
 
 app.listen(port, () => {
--- services/users.service.js
+++ services/users.service.js
@@ -1,4 +1,5 @@
 import database from '../firestore/config.cjs';
+import { v4 as uuidv4 } from 'uuid';
 const { db } = database;
 
 export class UsersService {
@@ -10,5 +11,10 @@ export class UsersService {
       this.users.push(doc.data());
     });
     return this.users;
+  };
+
+  addDataUser = async (objectData) => {
+    const respon = await db.collection('users').doc(uuidv4()).set(objectData);
+    return respon;
   };
 }
